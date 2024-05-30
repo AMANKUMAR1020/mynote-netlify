279b0c6ab4f7b42bd41c08135c21a0d7977f79fa
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2024-05-30 11:58:00 +0530

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,8 +3,8 @@ import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
 //    baseUrl: 'https://note-api-zi6e.onrender.com',
-//    baseUrl: 'https://mynote-api-netlify.onrender.com',
-    baseUrl: 'http://localhost:3500',
+    //baseUrl: 'http://localhost:3500',
+    baseUrl: 'https://mynote-api-netlify.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
